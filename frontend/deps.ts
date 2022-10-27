4a320f9bb79ebc757f91825ab082868c8cad2893
Usuyuki <63891531+Usuyuki@users.noreply.github.com>
2022-10-27 17:21:52 +0900

    wip react系のエラーが治らないので一部除いてgit resetでとばす

---
+++ frontend/deps.ts
@@ -0,0 +1,3 @@
+import { Env } from "https://deno.land/x/env@v2.2.0/env.js";
+const env = new Env();
+export { env };
---
+++ frontend/utils/twind.ts
@@ -0,0 +1,29 @@
+import { IS_BROWSER } from "$fresh/runtime.ts";
+import { Configuration, setup } from "twind";
+export * from "twind";
+import * as colors from "twind/colors";
+
+export const config: Configuration = {
+  darkMode: "class",
+  mode: "silent",
+  theme: {
+    colors: {
+      // ハイフン使えないのでキャメルケースに変換
+      kn_black: "#2f3437",
+      kn_white: "#f9fff9",
+      kn_d_purple: "#624464",
+      kn_d_blue: "#4b8996",
+      kb_d_brown: "#8a8772",
+      kn_l_purple: "#ac78b0",
+      kn_l_blue: "#71d0e3",
+      kn_l_brown: "#d6d2b2",
+      kn_s_1: "#33769c",
+      kn_s_2: "#339c76",
+      kn_s_3: "#e2534a",
+      kn_a_yellow: "#id7bd57",
+      knn_a_light_blue: "#d8e4ee",
+      kn_a_green: "#7e9e7f",
+    },
+  },
+};
+if (IS_BROWSER) setup(config);
